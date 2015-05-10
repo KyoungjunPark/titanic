40300fcb8dddb38c0d5ccda5b62a37b9c780b585
Park.Kyoung-Jun <pgj525@cau.ac.kr>
2015-05-10 21:44:12 +0900

    update menus & toolbar This commit is about overall layout.

--- src/titanic/Main.java
+++ src/titanic/Main.java
@@ -1,9 +1,98 @@
 package titanic;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
-	public static void main(String[] args) {
-		System.out.println("hello titanic!");
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JSplitPane;
+import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JToolBar;
+import javax.swing.JTabbedPane;
+import javax.swing.JPopupMenu;
+
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import javax.swing.BoxLayout;
+
+public class Main extends JFrame {
+	
+	public Main() {
+		setTitle("Titanic");
+		setSize(1400,800);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenu mnNewMenu = new JMenu("Open DSM...");
+		mnFile.add(mnNewMenu);
+		
+		JMenu mnMetrics = new JMenu("Metrics");
+		menuBar.add(mnMetrics);
+		
+		JMenu mnNewMenu_1 = new JMenu("Propagation Cost");
+		mnMetrics.add(mnNewMenu_1);
+		
+		JMenu mnView = new JMenu("View");
+		menuBar.add(mnView);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		
+		System.out.println(Main.class.getResource("").getPath());
+		
+		JToolBar toolBar = new JToolBar();
+		
+		ImageIcon opendsmIcon = new ImageIcon(Main.class.getResource("").getPath()+"../../util/open-dsm.png");
+		JButton opendsmButton = new JButton(opendsmIcon);
+		toolBar.add(opendsmButton);
+
+		add(toolBar, BorderLayout.NORTH);
 	}
-
+	public static void main(String[] args) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new Main().setVisible(true);
+				}
+			});
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
 }
