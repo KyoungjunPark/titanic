0a73f6f13a9219821e20916d5ddca6da9a73ec25
Han YeLim <dpfla0376@hanmail.net>
2015-05-13 12:58:27 +0900

    Leftscreen extends JPanel

--- src/titanic/LeftScreen.java
+++ src/titanic/LeftScreen.java
@@ -4,13 +4,10 @@ import java.awt.BorderLayout;
 
 import javax.swing.*;
 
-public class LeftScreen {
-	
-	JPanel leftScreen;
-	
-	public LeftScreen(){
+public class LeftScreen extends JPanel{
 		
-		leftScreen = new JPanel(new BorderLayout(0,0));
+	public LeftScreen(){
+		super(new BorderLayout(0,0));
 		makeToolbar();
 	}
 	protected boolean makeToolbar()
@@ -21,42 +18,36 @@ public class LeftScreen {
 		JToolBar toolBar2 = new JToolBar();
 		toolBar2.setFloatable(false);
 		
-		ImageIcon expandAllIcon = new ImageIcon(LeftScreen.class.getResource("").getPath()+"../../util/expand.png");
+		ImageIcon expandAllIcon = new ImageIcon("util/expand.png");
 		JButton expandAllButton = new JButton(expandAllIcon);
 		toolBar2.add(expandAllButton);
 		
-		ImageIcon collapseAllIcon = new ImageIcon(LeftScreen.class.getResource("").getPath()+"../../util/collapse.png");
+		ImageIcon collapseAllIcon = new ImageIcon("util/collapse.png");
 		JButton collapseAllButton = new JButton(collapseAllIcon);
 		toolBar2.add(collapseAllButton);
 		
-		ImageIcon groupIcon = new ImageIcon(LeftScreen.class.getResource("").getPath()+"../../util/group.png");
+		ImageIcon groupIcon = new ImageIcon("util/group.png");
 		JButton groupButton = new JButton(groupIcon);
 		toolBar2.add(groupButton);
 		
-		ImageIcon ungroupIcon = new ImageIcon(LeftScreen.class.getResource("").getPath()+"../../util/ungroup.png");
+		ImageIcon ungroupIcon = new ImageIcon("util/ungroup.png");
 		JButton ungroupButton = new JButton(ungroupIcon);
 		toolBar2.add(ungroupButton);
 		
-		ImageIcon moveUpIcon = new ImageIcon(LeftScreen.class.getResource("").getPath()+"../../util/up.png");
+		ImageIcon moveUpIcon = new ImageIcon("util/up.png");
 		JButton moveUpButton = new JButton(moveUpIcon);
 		toolBar2.add(moveUpButton);
 		
-		ImageIcon moveDownIcon = new ImageIcon(LeftScreen.class.getResource("").getPath()+"../../util/down.png");
+		ImageIcon moveDownIcon = new ImageIcon("util/down.png");
 		JButton moveDownButton = new JButton(moveDownIcon);
 		toolBar2.add(moveDownButton);
 		
-		ImageIcon deleteIcon = new ImageIcon(LeftScreen.class.getResource("").getPath()+"../../util/delete.png");
+		ImageIcon deleteIcon = new ImageIcon("util/delete.png");
 		JButton deleteButton = new JButton(deleteIcon);
 		toolBar2.add(deleteButton);
 		
-		leftScreen.add(toolBar2, BorderLayout.NORTH);
+		this.add(toolBar2, BorderLayout.NORTH);
 		return true;
-		
-	}
-
-	protected JPanel getLeftScreen()
-	{
-		return leftScreen;
 		
 	}
 }
