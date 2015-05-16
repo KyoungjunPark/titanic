40c6ad04b4e1221f8007811b72be93d8e161c1ea
Kim, Ji-Soo <jskim@bobcats.kr>
2015-05-16 23:24:53 +0900

    init createCLSXModel in filemanager

--- src/Model/FileManager.java
+++ src/Model/FileManager.java
@@ -3,6 +3,8 @@ package Model;
 public class FileManager {
 	private static FileManager fileManager;
 	private FileManager(){}
+	private int next_id = 1;
+	private int current_id = 0;
 	public static FileManager sharedFileManager(){
 		if(fileManager == null){
 			synchronized (FileManager.class) {
@@ -15,7 +17,27 @@ public class FileManager {
 	}
 	
 	public boolean setCLSXModel(int id){
-		
+		if(this.existModel(id)){
+			this.current_id = id;
+			return true;
+		}
+		return false;
+	}
+
+	public int createCLSXModel(){
+		return getNextID();
+	}
+	public int createCLSXModel(String fileName){
+		return createCLSXModel();
+	}
+	public int createCLSXModel(int id){
+		return createCLSXModel();
+	}
+	public boolean existModel(int id){
 		return true;
+	}
+	/* setter, getter */
+	private int getNextID(){
+		return this.next_id++;
 	}
 }
