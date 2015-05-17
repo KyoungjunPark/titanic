7e336179e370cb710c0698b3d429c6ef016a9e22
Park.Kyoung-Jun <pgj525@cau.ac.kr>
2015-05-17 14:20:49 +0900

    add keyboard shortcut feature

--- src/titanic/BackgroundPanel.java
+++ src/titanic/BackgroundPanel.java
@@ -1,6 +1,11 @@
 package titanic;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Event;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -8,15 +13,14 @@ public class BackgroundPanel extends JFrame {
 	
 	protected JMenuBar menuBar;
 	protected JMenu mnFile;
-	protected JMenu mnNewMenu;
 	protected JMenu mnMetrics;
-	protected JMenu mnNewMenu_1;
 	protected JMenu mnView;
 	protected JMenu mnHelp;
 	
 	protected JToolBar toolBar;
 
-	
+	JTextArea text;  
+	Container pane;
 	
 	public BackgroundPanel() {
 		setTitle("Titanic");
@@ -30,6 +34,7 @@ public class BackgroundPanel extends JFrame {
 		makeMainPanel();
 	
 	}
+	
 	protected void makeMenus()
 	{
 		//Menus
@@ -38,22 +43,102 @@ public class BackgroundPanel extends JFrame {
 		
 		mnFile = new JMenu("File");
 		menuBar.add(mnFile);
-		
-		mnNewMenu = new JMenu("Open DSM...");
-		mnFile.add(mnNewMenu);
+		mnFile.setMnemonic('F');
 		
 		mnMetrics = new JMenu("Metrics");
 		menuBar.add(mnMetrics);
-		
-		mnNewMenu_1 = new JMenu("Propagation Cost");
-		mnMetrics.add(mnNewMenu_1);
+		mnMetrics.setMnemonic('M');
 		
 		mnView = new JMenu("View");
 		menuBar.add(mnView);
+		mnView.setMnemonic('V');
 		
 		mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
+		mnHelp.setMnemonic('H');
+
 		
+		JMenuItem openDSM = new JMenuItem("Open DSM...", new ImageIcon("util/open-dsm.png"));
+        JMenuItem newClustering = new JMenuItem("New Clustering", new ImageIcon("util/new-clsx.png"));
+        JMenuItem loadClustering = new JMenuItem("Load Clustering...", new ImageIcon("util/open-clsx.png"));
+        JMenuItem saveClustering = new JMenuItem("Save Clustering", new ImageIcon("util/save-clsx.png"));
+        JMenuItem saveClustering_2 = new JMenuItem("Save Clustering...", new ImageIcon("util/save-clsx-as.png"));
+        JMenuItem exportAs = new JMenu("Export As");
+        JMenuItem exitAction = new JMenuItem("Exit");
+        
+        JMenuItem propagationCost= new JMenuItem("Propagation Cost");
+        
+        JMenuItem redraw = new JMenuItem("Redraw", new ImageIcon("util/redraw.png"));
+        JMenuItem find = new JMenuItem("Find...");
+        JCheckBoxMenuItem showRowLabels = new JCheckBoxMenuItem("Show Row Labels");
+        JCheckBoxMenuItem showDependencyStrength = new JCheckBoxMenuItem("Show Dependency Strength");
+        
+        JMenuItem about = new JMenuItem("About...");
+        
+        JMenuItem DSM = new JMenuItem("DSM...");
+        JMenuItem excel = new JMenuItem("Excel...");
+        
+        mnFile.add(openDSM);
+        mnFile.addSeparator();
+        mnFile.add(newClustering);
+        mnFile.add(loadClustering);
+        mnFile.addSeparator();
+        mnFile.add(saveClustering);
+        mnFile.add(saveClustering_2);
+        mnFile.addSeparator();
+        mnFile.add(exportAs);
+        mnFile.addSeparator();
+        mnFile.add(exitAction);
+        openDSM.setMnemonic('O');
+        newClustering.setMnemonic('N');
+        loadClustering.setMnemonic('L');
+        saveClustering.setMnemonic('S');
+        saveClustering_2.setMnemonic('a');
+        exportAs.setMnemonic('E');
+        exitAction.setMnemonic('x');
+        openDSM.setAccelerator(KeyStroke.getKeyStroke('O', Event.CTRL_MASK, false));
+        loadClustering.setAccelerator(KeyStroke.getKeyStroke('O', Event.CTRL_MASK + Event.SHIFT_MASK, false));
+        saveClustering.setAccelerator(KeyStroke.getKeyStroke('S', Event.CTRL_MASK, false));
+        
+        exportAs.add(DSM);
+        exportAs.add(excel);
+        DSM.setMnemonic('D');
+        excel.setMnemonic('E');
+        
+        mnMetrics.add(propagationCost);
+        propagationCost.setMnemonic('P');
+        
+        mnView.add(redraw);
+        mnView.addSeparator();
+        mnView.add(find);
+        mnView.addSeparator();
+        mnView.add(showRowLabels);
+        mnView.add(showDependencyStrength);
+        redraw.setMnemonic('R');
+        find.setMnemonic('F');
+        showRowLabels.setMnemonic('L');
+        showDependencyStrength.setMnemonic('D');
+        redraw.setAccelerator(KeyStroke.getKeyStroke(0, Event.F5, false));			//F5 단축키 설정 모르겠음
+        find.setAccelerator(KeyStroke.getKeyStroke('F', Event.CTRL_MASK, false));
+        
+        mnHelp.add(about);
+        about.setMnemonic('A');
+        //frame.getContentPane(  ).add(menuBar, BorderLayout.SOUTH);
+        
+        
+        about.addActionListener(new ActionListener()  								//불완전
+        {  
+         public void actionPerformed(ActionEvent e)  
+         {  
+        	 JPanel p1 = new JPanel();
+    		 p1.setLayout(new GridLayout(3, 1));
+    		 p1.add(new JLabel("Titan"));
+    		 p1.add(new JLabel("version 1.0"));
+    		 p1.add(new JLabel("Copyright(c) 2009-2015, Drexel University"));
+         JOptionPane.showMessageDialog(pane, p1);
+         }  
+        });   
+       
 	}
 	protected void makeOverallToolbar()
 	{
