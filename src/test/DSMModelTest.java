a430f18c93f4796124539774356096bbaa6beac8
KimJiSoo <A@KimJiSooui-MacBook.local>
2015-05-18 21:06:05 +0900

    add some test case

--- src/test/DSMModelTest.java
+++ src/test/DSMModelTest.java
@@ -1,13 +1,19 @@
 package test;
 
 import static org.junit.Assert.*;
+import model.CreateException;
+import model.DSMModel;
 
 import org.junit.Test;
 public class DSMModelTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		try {
+			DSMModel model = new DSMModel("2\n1 2\n3 4\nAA\nBB");
+		} catch (CreateException e) {
+			fail("Not yet implemented");
+		}
 	}
 
 }
