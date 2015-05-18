a60fd5702c8e1eef4351768d5e78c14655af9921
Kim, Ji-Soo <jskim@bobcats.kr>
2015-05-18 23:55:16 +0900

    add dsmmodeltest

--- src/test/DSMModelTest.java
+++ src/test/DSMModelTest.java
@@ -8,15 +8,33 @@ import org.junit.Test;
 public class DSMModelTest {
 
 	@Test
-	public void test() {
+	public void DSMModeltest() {
 		try {
 			String string = "2\n1 2\n3 4\nAA\nBB";
 			DSMModel model = new DSMModel(string);
-			
-			assertEquals(string, model);
+			assertTrue(string.equals(model.toString()));
 		} catch (CreateException e) {
 			fail("Not yet implemented");
 		}
+        try {
+            String string = "3\n1 2 3\n3 4 6\n7 8 9\nAA\nBB\nCC";
+            DSMModel model = new DSMModel(string);
+            assertTrue(string.equals(model.toString()));
+        } catch (CreateException e) {
+            fail("Not yet implemented");
+        }
+        try {
+            String string = "5\n1 2\n3 4\nAA\nBB";
+            DSMModel model = new DSMModel(string);
+            fail("Not yet implemented");
+        } catch (CreateException e) {
+        }
+        try {
+            String string = "2\n1 2 3\n3 \nAA\nBB";
+            DSMModel model = new DSMModel(string);
+            fail("Not yet implemented");
+        } catch (CreateException e) {
+        }
 	}
 
 }
