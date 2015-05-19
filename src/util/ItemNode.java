23bf6cf839f2ee74419b9b9362da2693db70cebf
Kim, Ji-Soo <jskim@bobcats.kr>
2015-05-19 22:41:22 +0900

    add ParendNode in node

--- src/util/ItemNode.java
+++ src/util/ItemNode.java
@@ -7,8 +7,15 @@ public class ItemNode extends Node{
     public ItemNode(){
 
     }
-    public ItemNode(String name){
+    public ItemNode(Node node, String name){
+        this.setParentNode(node);
         this.setName(name);
+    }
+    public ItemNode(Node node){
+        this(node, "");
+    }
+    public ItemNode(String name){
+        this(null, name);
     }
     public void print(){
         System.out.println(this);
