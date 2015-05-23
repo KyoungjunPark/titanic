e5adee73fcc84050ccc2ae3b24e960a208473ec4
Park.KyoungJun <pgj525@cau.ac.kr>
2015-05-24 00:45:14 +0900

    perfect up & down feature lol

--- src/titanic/FileTree.java
+++ src/titanic/FileTree.java
@@ -10,12 +10,15 @@ import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 
 import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory.Default;
 
 import util.GroupNode;
 import util.TreeNode;
+import model.EventManager;
 import model.ModelManager;
 
 public class FileTree extends JTree implements Controllerable {
@@ -27,63 +30,128 @@ public class FileTree extends JTree implements Controllerable {
 	}
 
 	public void makeTree() {
-		ModelManager.sharedModelManager().setTitanicModelID(0); //must be deleted!!!!
-		
+		ModelManager.sharedModelManager().setTitanicModelID(0); // must be
+																// deleted!!!!
+
 		root = ModelManager.sharedModelManager().getCurrentTitanicModel()
 				.getGroupNode().getTreeNode();
 		setModel(new DefaultTreeModel(root));
 	}
-	protected void moveUp()
-	{
-		ArrayList<DefaultMutableTreeNode> nodes = getSelectedNodes();
-		
-		for(int i = 0 ; i <nodes.size(); i++){
-			DefaultMutableTreeNode node = nodes.get(i);
-			if(node.getPreviousLeaf() == null){
-				
-				
-			}
-			//((DefaultTreeModel)this.getModel()).
-			
-		}
-		
-	}
-	protected void expandAll(FileTree tree, int startingIndex, int rowCount) {
-		for(int i=startingIndex; i<rowCount; i++){
-	        tree.expandRow(i);
-	    }
 
-	    if(tree.getRowCount()!= rowCount){
-	        expandAll(tree, rowCount, tree.getRowCount());
-	    }
+	protected void moveUp() {
+
+		ArrayList<DefaultMutableTreeNode> nodes = getSelectedNodes();
+
+		System.out.println(nodes.size());
+		for (int i = 0; i < nodes.size(); i++) {
+			DefaultMutableTreeNode node = nodes.get(i);
+
+			DefaultTreeModel model = (DefaultTreeModel) this.getModel();
+			DefaultMutableTreeNode root = (DefaultMutableTreeNode) model
+					.getRoot();
+			DefaultMutableTreeNode newNode = new DefaultMutableTreeNode();
+
+			newNode = (DefaultMutableTreeNode) deepClone(node);
+			model.insertNodeInto(newNode, root,
+					node.getParent().getIndex(node) - 1);
+
+			node.removeFromParent();
+
+			model.reload(root);
+
+			/*
+			 * DefaultMutableTreeNode node = nodes.get(i);
+			 * 
+			 * ((DefaultTreeModel)this.getModel()).insertNodeInto((MutableTreeNode
+			 * )node, (MutableTreeNode)node.getParent() ,
+			 * node.getParent().getIndex(node) - 1);
+			 * 
+			 * System.out.println(node.getParent().getIndex(node)+1);
+			 * ((DefaultMutableTreeNode
+			 * )node.getParent()).remove((node.getParent().getIndex(node)+1));
+			 * 
+			 * node.getParent().remove();
+			 */
+		}
+
 	}
+
+	private DefaultMutableTreeNode deepClone(DefaultMutableTreeNode source) {
+		DefaultMutableTreeNode newNode = (DefaultMutableTreeNode) source
+				.clone();
+		Enumeration enumeration = source.children();
+		while (enumeration.hasMoreElements()) {
+			newNode.add(deepClone((DefaultMutableTreeNode) enumeration
+					.nextElement()));
+		}
+		return newNode;
+	} // end of deepClone
+
+	public Object copySubTree(DefaultMutableTreeNode subRoot,
+			DefaultMutableTreeNode sourceTree)
+			throws CloneNotSupportedException {
+		if (sourceTree == null || sourceTree.isLeaf()) {
+			return new DefaultMutableTreeNode(sourceTree);
+		}
+		for (int i = 0; i < sourceTree.getChildCount(); i++) {
+			DefaultMutableTreeNode child = (DefaultMutableTreeNode) sourceTree
+					.getChildAt(i);
+			DefaultMutableTreeNode clone = new DefaultMutableTreeNode(
+					child.getUserObject());
+			subRoot.add(clone);
+			copySubTree(clone, child);
+		}
+		return subRoot;
+	}
+
+	protected void addItem() {
+		DefaultTreeModel model = (DefaultTreeModel) this.getModel();
+		DefaultMutableTreeNode root = (DefaultMutableTreeNode) model.getRoot();
+		root.add(new DefaultMutableTreeNode("another_child"));
+		model.reload(root);
+
+	}
+
+	protected void expandAll(FileTree tree, int startingIndex, int rowCount) {
+		for (int i = startingIndex; i < rowCount; i++) {
+			tree.expandRow(i);
+		}
+
+		if (tree.getRowCount() != rowCount) {
+			expandAll(tree, rowCount, tree.getRowCount());
+		}
+	}
+
 	protected void collapseAll(FileTree tree, int startingIndex, int rowCount) {
-		for(int i=rowCount-1; i>=startingIndex; i--) {
+		for (int i = rowCount - 1; i >= startingIndex; i--) {
 			tree.collapseRow(i);
 		}
 
 	}
+
 	protected void delete() {
-		
+
 		ArrayList<DefaultMutableTreeNode> nodes = getSelectedNodes();
-		for(int i = 0 ; i< nodes.size(); i++){
-			((DefaultTreeModel)this.getModel()).removeNodeFromParent(nodes.get(i));
+		for (int i = 0; i < nodes.size(); i++) {
+			((DefaultTreeModel) this.getModel()).removeNodeFromParent(nodes
+					.get(i));
 		}
-		
+
 	}
-	private ArrayList<DefaultMutableTreeNode> getSelectedNodes()
-	{
+
+	private ArrayList<DefaultMutableTreeNode> getSelectedNodes() {
 		TreePath[] paths = this.getSelectionPaths();
 		ArrayList<DefaultMutableTreeNode> nodes = new ArrayList<DefaultMutableTreeNode>();
-		for(TreePath path : paths){
-			nodes.add((DefaultMutableTreeNode)path.getLastPathComponent());
+		for (TreePath path : paths) {
+			nodes.add((DefaultMutableTreeNode) path.getLastPathComponent());
 		}
-		
+
 		return nodes;
-		
+
 	}
+
 	public DefaultMutableTreeNode findNode(String search) {
-		
+
 		Enumeration nodeEnumeration = root.breadthFirstEnumeration();
 		while (nodeEnumeration.hasMoreElements()) {
 			DefaultMutableTreeNode node = (DefaultMutableTreeNode) nodeEnumeration
@@ -96,7 +164,6 @@ public class FileTree extends JTree implements Controllerable {
 		return null;
 	}
 
-	
 	@Override
 	public void setAction(String title, ActionListener action) {
 
--- src/titanic/FileTreeController.java
+++ src/titanic/FileTreeController.java
@@ -1,6 +1,5 @@
 package titanic;
 
-
 import javax.swing.JOptionPane;
 import javax.swing.JTree;
 import javax.swing.event.TreeSelectionEvent;
@@ -12,12 +11,11 @@ import javax.swing.tree.TreeSelectionModel;
 import model.EventManager;
 import util.TreeNode;
 
-
 public class FileTreeController extends LeftPanelController {
 
 	private FileTree treeFile;
 	private TreePath[] selectedPaths;
-	
+
 	public FileTreeController(FileTree treeFile) {
 		this.treeFile = treeFile;
 
@@ -25,46 +23,62 @@ public class FileTreeController extends LeftPanelController {
 	}
 
 	private void init() {
-		
+
 		treeFile.addTreeSelectionListener(new TreeSelectionListener() {
-			
+
 			@Override
 			public void valueChanged(TreeSelectionEvent e) {
-				DefaultMutableTreeNode node = (DefaultMutableTreeNode) e.getPath().getLastPathComponent();
-				
-				if(node.isRoot()){
+				DefaultMutableTreeNode node = (DefaultMutableTreeNode) e
+						.getPath().getLastPathComponent();
+
+				if (node.isRoot()) {
 					EventManager.callEvent("changeRootStatus");
-					
-				}else if(node.isLeaf()){
-					System.out.println("leaf & top leaf : " + node.getPreviousLeaf());
-					System.out.println("leaf & Bottom leaf : "+node.getNextLeaf());
-					System.out.println("getFirstLeaf : " + ((DefaultMutableTreeNode)node.getParent()).getFirstChild());
-					System.out.println("getLastLeaf : " + ((DefaultMutableTreeNode)node.getParent()).getLastChild());
-					if(node.getPreviousLeaf() == null || ((DefaultMutableTreeNode)node.getParent()).getFirstChild() == node){ 
-						//if this node is first leaf of parent's(disable move up icon)
+
+				} else if (node.isLeaf()) {
+					/*
+					 * test version System.out.println("leaf & top leaf : " +
+					 * node.getPreviousLeaf());
+					 * System.out.println("leaf & Bottom leaf : "
+					 * +node.getNextLeaf());
+					 * System.out.println("getFirstLeaf : " +
+					 * ((DefaultMutableTreeNode
+					 * )node.getParent()).getFirstChild());
+					 * System.out.println("getLastLeaf : " +
+					 * ((DefaultMutableTreeNode
+					 * )node.getParent()).getLastChild());
+					 */
+					if (node.getPreviousLeaf() == null
+							|| ((DefaultMutableTreeNode) node.getParent())
+									.getFirstChild() == node) {
+						// if this node is first leaf of parent's(disable move
+						// up icon)
 						EventManager.callEvent("changeItemStatusTop");
-					}
-					else if(node.getNextLeaf() == null || ((DefaultMutableTreeNode)node.getParent()).getLastChild() == node){
-						//if this node is last leaf of parent's(disable move down icon)
+					} else if (node.getNextLeaf() == null
+							|| ((DefaultMutableTreeNode) node.getParent())
+									.getLastChild() == node) {
+						// if this node is last leaf of parent's(disable move
+						// down icon)
 						EventManager.callEvent("changeItemStatusBottom");
-					}
-					else{
+					} else {
 						EventManager.callEvent("changeItemStatus");
 					}
-					
-				}else{
-					System.out.println("Node & top Node : " + node.getPreviousSibling());
-					System.out.println("Node & Bottom Node : "+node.getNextSibling());
-					if(node.getPreviousSibling() == null){
-						//if this node is first node of parent's(disable move up icon)
+
+				} else {
+					System.out.println("Node & top Node : "
+							+ node.getPreviousSibling());
+					System.out.println("Node & Bottom Node : "
+							+ node.getNextSibling());
+					if (node.getPreviousSibling() == null) {
+						// if this node is first node of parent's(disable move
+						// up icon)
 						EventManager.callEvent("changeSubRootStatusTop");
-						
-					}
-					else if(node.getNextSibling() == null ){
-						//if this node is first node of parent's(disable move up icon)
+
+					} else if (node.getNextSibling() == null) {
+						// if this node is first node of parent's(disable move
+						// up icon)
 						EventManager.callEvent("changeSubRootStatusBottom");
-						
-					}else{
+
+					} else {
 						EventManager.callEvent("changeSubRootStatus");
 					}
 				}
@@ -76,21 +90,23 @@ public class FileTreeController extends LeftPanelController {
 	protected void makeTree() {
 		treeFile.makeTree();
 	}
-	protected void moveUp()
-	{
+
+	protected void moveUp() {
 		treeFile.moveUp();
 	}
+
 	protected void expandAll(FileTree tree, int startingIndex, int rowCount) {
 		treeFile.expandAll(tree, startingIndex, rowCount);
-	    
+
 	}
+
 	protected void collapseAll(FileTree tree, int startingIndex, int rowCount) {
 		treeFile.collapseAll(tree, startingIndex, rowCount);
 
 	}
+
 	protected void delete() {
 		treeFile.delete();
 	}
-
 
 }
