ba52bb8810c9fcfc0edbb12084872023d792e445
Park.KyoungJun <pgj525@cau.ac.kr>
2015-05-25 22:58:03 +0900

    delete notice message

--- src/titanic/MenuBarController.java
+++ src/titanic/MenuBarController.java
@@ -158,8 +158,7 @@ public class MenuBarController extends MainController{
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Redraw is clicked");
-				
+				EventManager.callEvent("Redraw");
 				// 타이타닉 모델이 하나라도 생성되어 있다면 리드로우 해줌
 				//if(ModelManager.sharedModelManager().getTitanicModelCount()!=0)
 				//	EventManager.callEvent("Redraw");
