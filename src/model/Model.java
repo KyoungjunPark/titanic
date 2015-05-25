28408d3ab1955b31bfbab7ddd4d0e5f018b159d6
Kim, Ji-Soo <jskim@bobcats.kr>
2015-05-25 15:01:11 +0900

    get filename

--- src/model/Model.java
+++ src/model/Model.java
@@ -11,7 +11,12 @@ abstract class Model {
     public String getFilePath() {
         return filePath;
     }
-    public String getFileName() { return filePath.substring(filePath.lastIndexOf("/"));}
+    public String getFileName() {
+        if(filePath.lastIndexOf("/") != -1)
+            return filePath.substring(filePath.lastIndexOf("/"));
+        else
+            return filePath.substring(filePath.lastIndexOf("\\"));
+    }
     public void setFilePath(String filePath) {
         this.filePath = filePath;
     }
