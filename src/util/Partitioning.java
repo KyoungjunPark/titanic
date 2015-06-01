aa1bab11d73d473b0f4e900553ef72a532e8d64b
Park.Kyoung-Jun <pgj525@cau.ac.kr>
2015-06-02 00:14:44 +0900

    opening of YASSINE...

--- src/util/Partitioning.java
+++ src/util/Partitioning.java
@@ -1,5 +1,44 @@
 package util;
 
+import java.util.ArrayList;
+
 public class Partitioning {
 
+    private int dependencyNumber;
+    private ArrayList<Integer> dependencyRelationArray;
+    private ArrayList<String> elementsNameArray;
+
+    public Partitioning(int dependencyNumber, ArrayList<Integer> dependencyRelationArray, ArrayList<String> elementsNameArray){
+        this.dependencyNumber = dependencyNumber;
+        this.dependencyRelationArray = dependencyRelationArray;
+        this.elementsNameArray = elementsNameArray;
+
+        doPartitioning();
+    }
+    private void doPartitioning()
+    {
+
+    }
+    private ArrayList<Integer> moveUpORLeft()
+    {
+
+    }
+    private ArrayList<Integer> moveDownORRight()
+    {
+
+    }
+    private ArrayList<Integer> moveRowToBottom(int row){
+
+    }
+    private ArrayList<Integer> moveRowToTop(int row){
+
+    }
+    private ArrayList<Integer> nthSquare()
+    {
+
+    }
+    private void makeGroupNode()
+    {
+
+    }
 }
